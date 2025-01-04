9adea713f7e5ca79fa0b7077bf9403c1907e3d11
Thiago Goetten <123814831+ThiagoGoetten@users.noreply.github.com>
2025-01-04 17:06:33 +0000

    Adicionando `infra` e Modificando para BD

---
+++ infra/database.js
@@ -0,0 +1,19 @@
+import { Client, Query } from "pg";
+
+async function query(queryObject) {
+  const client = new Client({
+    host: "localhost",
+    port: 5432,
+    user: "postgres",
+    database: "postgres",
+    password: "10141823",
+  });
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
+}
+
+export default {
+  query: query,
+};
